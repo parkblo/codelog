199397e5a47eecd7f45aa136d9ae9729815ddcf1
Park Byeongju <beyondzux@gmail.com>
2025-11-30 15:37:02 +0900

    feat: 홈 피드에서 게시글 작성 진입을 위한 PostCard 컴포넌트 구현 (#7)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,5 @@
 import Post from "@/components/home/Post";
+import PostCard from "@/components/home/PostCard";
 import WelcomeCard from "@/components/home/WelcomeCard";
 
 export default async function HomePage() {
@@ -127,6 +128,7 @@ fn calculate_length(s: &String) -> usize {
   return (
     <div className="p-4 space-y-4">
       <WelcomeCard />
+      <PostCard />
       {mockPosts.map((post) => (
         <Post key={post.id} post={post} fullPage={false} />
       ))}
---
+++ src/components/home/PostCard.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useAuth } from "@/providers/auth-provider";
+import { Card, CardContent } from "../ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
+import React from "react";
+
+/**
+ * 게시글 피드 화면 상단에 표출되어, 사용자가 게시글을 작성할 수 있도록 하는 컴포넌트
+ * @returns PostCard
+ */
+export default function PostCard() {
+  const { user, loading } = useAuth();
+
+  if (!user || loading) {
+    return null;
+  }
+
+  const openDialog = (
+    e:
+      | React.FocusEvent<HTMLTextAreaElement>
+      | React.MouseEvent<HTMLButtonElement>
+  ) => {
+    // TODO - 게시글 작성 모달을 띄우기 위한 함수
+    if (e.target instanceof HTMLTextAreaElement) {
+      e.target.blur();
+    }
+  };
+
+  return (
+    <Card className="bg-background">
+      <CardContent>
+        <div className="flex gap-2 items-center">
+          <Avatar className="w-10 h-10 border border-border">
+            <AvatarImage src={user.avatar} alt={user.nickname} />
+            <AvatarFallback>
+              {user.nickname ? user.nickname.charAt(0) : ""}
+            </AvatarFallback>
+          </Avatar>
+          <Textarea
+            placeholder="무엇을 공유할까요?"
+            className="resize-none min-h-4"
+            onFocus={openDialog}
+          />
+          <Button variant="outline" onClick={openDialog}>
+            게시
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}

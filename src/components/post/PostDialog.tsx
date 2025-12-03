53952ef29d4ce920860b8cf3ddecd34c3ea49bd8
Byeongju Park <badmuxi@gmail.com>
2025-12-03 16:42:35 +0900

    feat: /home 페이지 및 포스트 다이얼로그 컴포넌트에 서버 액션 연결 (#7)

--- src/components/post/PostDialog.tsx
+++ src/components/post/PostDialog.tsx
@@ -6,11 +6,12 @@ import { Textarea } from "../ui/textarea";
 import { useAuth } from "@/providers/auth-provider";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
-import { Code2, Plus } from "lucide-react";
+import { Code2, Loader, Plus, Send } from "lucide-react";
 import { CodeEditor } from "../ui/code-editor";
 import { TagList } from "../ui/tag-list";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { createPostAction } from "@/actions/post.action";
 
 interface PostDialogProps {
   isOpen: boolean;
@@ -18,11 +19,15 @@ interface PostDialogProps {
 }
 
 export default function PostDialog({ isOpen, handleClose }: PostDialogProps) {
-  const [content, setContent] = useState("");
   const { user, loading } = useAuth();
+  const [content, setContent] = useState("");
   const [snippetMode, setSnippetMode] = useState(false);
   const [tags, setTags] = useState<string[]>([]);
   const [tagInput, setTagInput] = useState("");
+  const [codeInput, setCodeInput] = useState("");
+  const [languageInput, setLanguageInput] = useState("");
+  const [errorText, setErrorText] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const toggleSnippetMode = () => {
     setSnippetMode(snippetMode ? false : true);
@@ -60,6 +65,30 @@ export default function PostDialog({ isOpen, handleClose }: PostDialogProps) {
   const deleteTag = (tag: string) => {
     console.log(tag);
     setTags((prev) => prev.filter((t) => t !== tag));
+  };
+
+  const handleSubmit = async () => {
+    if (!user || isSubmitting) return;
+
+    setIsSubmitting(true);
+    setErrorText(null);
+
+    const { data, error } = await createPostAction({
+      author: user,
+      content,
+      code: snippetMode ? codeInput : null,
+      language: snippetMode ? languageInput : null,
+      tags,
+    });
+
+    if (error) {
+      setErrorText(error);
+    } else {
+      handleClose();
+      setContent("");
+      setTags([]);
+    }
+    setIsSubmitting(false);
   };
 
   return (
@@ -108,6 +137,26 @@ export default function PostDialog({ isOpen, handleClose }: PostDialogProps) {
             <div>
               <TagList tags={tags} onDelete={deleteTag} />
             </div>
+            <div className="flex justify-end">
+              <Button
+                className="w-auto"
+                onClick={handleSubmit}
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? (
+                  <span className="flex items-center gap-2">
+                    <Loader className="w-4 h-4" />
+                    게시 중...
+                  </span>
+                ) : (
+                  <span className="flex items-center gap-2">
+                    <Send className="w-4 h-4" />
+                    게시
+                  </span>
+                )}
+              </Button>
+            </div>
+            <span className="text-color-red">{errorText}</span>
           </div>
         </div>
       </DialogContent>

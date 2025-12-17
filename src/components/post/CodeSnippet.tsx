01947a1c9d092b4b2533f84aa6739d492ddcdccc
Park Byeongju <beyondzux@gmail.com>
2025-12-17 16:08:19 +0900

    feat(ui): 코드 스니펫이 더 많은 언어에 하이라이팅이 가능하도록 모듈 import 추가 (#20)

--- src/components/post/CodeSnippet.tsx
+++ src/components/post/CodeSnippet.tsx
@@ -1,4 +1,22 @@
 import { Highlight, themes } from "prism-react-renderer";
+import Prism from "prismjs";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-csharp";
+import "prismjs/components/prism-go";
+import "prismjs/components/prism-rust";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-markup";
+import "prismjs/components/prism-markdown";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { Check, Copy } from "lucide-react";
@@ -147,7 +165,12 @@ export function CodeSnippet({
         </button>
       </div>
 
-      <Highlight theme={themes.vsDark} code={code} language={language}>
+      <Highlight
+        theme={themes.vsDark}
+        code={code}
+        language={language}
+        prism={Prism}
+      >
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
           <pre
             className={cn(className, "py-2 overflow-x-hidden text-sm")}

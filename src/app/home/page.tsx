434a9b8dc6b6e5911fb70acf52858eae41b46896
Park Byeongju <beyondzux@gmail.com>
2026-01-05 16:32:03 +0900

    fix: getPostsAction 에러 반환 타입 변경에 따른 표시 방식 결정 로직 변경 (#10)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -11,7 +11,7 @@ export default async function HomePage() {
       <div className="p-4 space-y-4">
         <WelcomeCard />
         <PostCard />
-        <span>{error instanceof Error ? error.message : error}</span>
+        <span>{error}</span>
       </div>
     );
   }

836dbc41bff897d2cdeefee1a42f65a8c31a4897
Park Byeongju <beyondzux@gmail.com>
2026-01-06 11:13:26 +0900

    feat: 게시글, 댓글에서 프로필 이동 기능 구현 (#10)

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,7 +2,7 @@ import SignOutButton from "./_components/SignOutButton";
 
 export default function SettingsPage() {
   return (
-    <div>
+    <div className="p-4 space-y-4">
       <SignOutButton />
     </div>
   );

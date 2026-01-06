5d96aa06d9449aef2fa778f08b08a7b230632478
Park Byeongju <beyondzux@gmail.com>
2026-01-06 11:04:59 +0900

    feat: 유저 아이디로 유저 정보를 가져오는 서비스 함수 구현 (#10)

--- src/services/user/user.interface.ts
+++ src/services/user/user.interface.ts
@@ -1,7 +1,20 @@
 import { UserAuth } from "@/types/types";
 
 export interface IUserService {
+  /**
+   * 사용자 정보를 수정합니다.
+   * @param user 사용자 정보
+   * @returns 에러
+   */
   editUser(
     user: Pick<UserAuth, "id" | "nickname" | "bio" | "avatar">
   ): Promise<{ error: Error | null }>;
+  /**
+   * username으로 사용자 정보를 가져옵니다.
+   * @param username 사용자 이름
+   * @returns 사용자 정보와 에러
+   */
+  getUserByUsername(
+    username: string
+  ): Promise<{ data: UserAuth | null; error: Error | null }>;
 }

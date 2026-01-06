87980c92817e235f24a9bf8964d4f248d834f5eb
Park Byeongju <beyondzux@gmail.com>
2026-01-06 10:22:43 +0900

    feat: 프로필 수정 모달 UI 구현 (#10)

--- src/app/profile/_components/MyProfileCard.tsx
+++ src/app/profile/_components/MyProfileCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { UserAuth } from "@/types/types";
+import ProfileEditDialog from "./ProfileEditDialog";
 
 interface MyProfileCardProps {
   user: UserAuth;
@@ -10,7 +11,13 @@ export default function MyProfileCard({ user }: MyProfileCardProps) {
   return (
     <Card>
       <CardContent>
-        <div className="flex flex-col items-center justify-center gap-4">
+        <div className="relative flex flex-col items-center justify-center gap-4">
+          {user && (
+            <div className="absolute top-2 right-2">
+              <ProfileEditDialog user={user} />
+            </div>
+          )}
+
           <Avatar className="w-30 h-30 border border-border">
             {/* 사용자 프로필 이미지 */}
             {user && (
@@ -25,7 +32,7 @@ export default function MyProfileCard({ user }: MyProfileCardProps) {
           <div className="flex flex-col items-center justify-center gap-1">
             <p className="font-semibold">{user?.nickname}</p>
             <p className="text-sm text-muted-foreground">@{user?.username}</p>
-            <p className="text-sm">{user?.bio}this is mock bio</p>
+            <p className="text-sm">{user?.bio}</p>
           </div>
         </div>
       </CardContent>

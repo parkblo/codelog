9df6ca8084ac3bdaa7ce5b77d5c4241fbe210d67
Park Byeongju <beyondzux@gmail.com>
2026-01-07 16:40:37 +0900

    feat: 유저 아바타 업로드 구현 (#10)

---
+++ src/services/user/user.service.browser.ts
@@ -0,0 +1,36 @@
+import { createClient } from "@/utils/supabase/client";
+import { IUserServiceBrowser } from "./user.interface";
+
+export class UserServiceBrowser implements IUserServiceBrowser {
+  async uploadAvatar(userId: string, file: File) {
+    const supabase = createClient();
+
+    const { data: currentUser, error: currentUserError } =
+      await supabase.auth.getUser();
+
+    if (currentUserError) {
+      return { data: null, error: "로그인이 필요합니다." };
+    }
+
+    if (currentUser.user.id !== userId) {
+      return { data: null, error: "본인의 정보만 수정할 수 있습니다." };
+    }
+
+    const { data, error } = await supabase.storage
+      .from("avatars")
+      .upload(`${userId}/${userId}`, file, {
+        cacheControl: "3600",
+        upsert: true,
+      });
+
+    if (error) {
+      return { data: null, error: error.message };
+    }
+
+    const {
+      data: { publicUrl },
+    } = supabase.storage.from("avatars").getPublicUrl(data.path);
+
+    return { data: publicUrl, error: null };
+  }
+}

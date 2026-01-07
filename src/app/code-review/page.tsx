8ca0319fe5539b52956b10e0ffd0d82e328bef87
Park Byeongju <beyondzux@gmail.com>
2026-01-07 17:23:18 +0900

    feat(code-review): 코드리뷰 페이지 헤더 UI 추가 (#10)

--- src/app/code-review/page.tsx
+++ src/app/code-review/page.tsx
@@ -2,6 +2,7 @@ import WelcomeCard from "@/components/home/WelcomeCard";
 import PostCard from "@/components/home/PostCard";
 import Post from "@/components/home/Post";
 import { getPostsAction } from "@/actions/post.action";
+import { MessageSquare } from "lucide-react";
 
 export default async function CodeReviewPage() {
   const { data, error } = await getPostsAction({ isReviewEnabled: true });
@@ -12,6 +13,14 @@ export default async function CodeReviewPage() {
 
   return (
     <div className="p-4 space-y-4">
+      <div className="flex items-center justify-between gap-2">
+        <h1 className="text-xl font-bold px-2 flex gap-2 items-center">
+          <MessageSquare /> 코드 리뷰
+        </h1>
+        <p className="text-sm text-muted-foreground">
+          훈수를 환영하는 게시글 목록입니다.
+        </p>
+      </div>
       <WelcomeCard />
       <PostCard />
       {data.map((post) => (

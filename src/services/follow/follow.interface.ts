bf0064c30520e3476e7c6d85386d135401806130
Park Byeongju <beyondzux@gmail.com>
2026-01-08 15:59:23 +0900

    feat(follow): 팔로우 서비스 구현 (#32)

---
+++ src/services/follow/follow.interface.ts
@@ -0,0 +1,49 @@
+import { Author } from "@/types/types";
+
+export interface IFollowService {
+  /**
+   * 사용자를 팔로우합니다.
+   * @param followerId 팔로우를 하는 사용자 ID
+   * @param followingId 팔로우를 받는 사용자 ID
+   */
+  follow(
+    followerId: string,
+    followingId: string
+  ): Promise<{ error: Error | null }>;
+
+  /**
+   * 팔로우를 취소합니다.
+   * @param followerId 팔로우를 취소하는 사용자 ID
+   * @param followingId 팔로우를 취소 당하는(?) 사용자 ID
+   */
+  unfollow(
+    followerId: string,
+    followingId: string
+  ): Promise<{ error: Error | null }>;
+
+  /**
+   * 특정 사용자를 팔로우하는 사람들의 목록을 가져옵니다.
+   * @param userId 대상 사용자 ID
+   */
+  getFollowers(
+    userId: string
+  ): Promise<{ data: Author[] | null; error: Error | null }>;
+
+  /**
+   * 특정 사용자가 팔로우하고 있는 사람들의 목록을 가져옵니다.
+   * @param userId 대상 사용자 ID
+   */
+  getFollowing(
+    userId: string
+  ): Promise<{ data: Author[] | null; error: Error | null }>;
+
+  /**
+   * 팔로우 여부를 확인합니다.
+   * @param followerId 팔로우 여부를 확인하는 사용자 ID
+   * @param followingId 대상 사용자 ID
+   */
+  isFollowing(
+    followerId: string,
+    followingId: string
+  ): Promise<{ data: boolean; error: Error | null }>;
+}

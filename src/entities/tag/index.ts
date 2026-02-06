45a56f5529d2ef9c607613a94620acbfb1910a6c
Byeongju Park <beyondzux@gmail.com>
2026-02-06 21:20:31 +0900

    refactor(entities): tag/index.ts에서 서버 전용 export 분리

--- src/entities/tag/index.ts
+++ src/entities/tag/index.ts
@@ -1,3 +1,4 @@
 export * from "./api/tag.action";
 export * from "./api/tag.interface";
-export * from "./api/tag.service";
+// Server-only exports는 ./server.ts에서 import하세요
+// import { TagService } from "@/entities/tag/server";
